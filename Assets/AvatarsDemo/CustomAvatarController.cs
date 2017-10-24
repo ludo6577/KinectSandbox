19c4b9ab6560350ce29419e4e445b258a3a54b3b
ludo6577 <ludo6577@gmail.com>
2017-10-24 13:59:44 +0200

    Init

---
+++ Assets/AvatarsDemo/CustomAvatarController.cs
@@ -0,0 +1,317 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomAvatarController : MonoBehaviour
+{
+
+    // Whether the offset node must be repositioned to the user's coordinates, as reported by the sensor or not.
+    public bool offsetRelativeToSensor = false;
+
+    // private instance of the KinectManager
+    protected KinectManager kinectManager;
+
+    // The body root node
+    protected Transform bodyRoot;
+
+    // Variable to hold all them bones. It will initialize the same size as initialRotations.
+    protected Transform[] bones;
+
+
+    // Initial position and rotation of the transform
+    protected Vector3 initialPosition;
+    protected Quaternion initialRotation;
+
+    // Rotations of the bones when the Kinect tracking starts.
+    protected Quaternion[] initialRotations;
+    protected Quaternion[] initialLocalRotations;
+    
+
+    // A required variable if you want to rotate the model in space.
+    protected GameObject offsetNode;
+
+    // Calibration Offset Variables for Character Position.
+    protected bool offsetCalibrated = false;
+    protected float xOffset, yOffset, zOffset;
+
+    // Rate at which avatar will move through the scene. The rate multiplies the movement speed (.001f, i.e dividing by 1000, unity's framerate).
+    protected int moveRate = 1;
+
+    // dictionaries to speed up bones' processing
+    // the author of the terrific idea for kinect-joints to mecanim-bones mapping
+    // along with its initial implementation, including following dictionary is
+    // Mikhail Korchun (korchoon@gmail.com). Big thanks to this guy!
+    private readonly Dictionary<int, HumanBodyBones> boneIndex2MecanimMap = new Dictionary<int, HumanBodyBones>
+    {
+        {0, HumanBodyBones.Hips},
+        {1, HumanBodyBones.Spine},
+        {2, HumanBodyBones.Neck},
+        {3, HumanBodyBones.Head},
+
+        {4, HumanBodyBones.LeftShoulder},
+        {5, HumanBodyBones.LeftUpperArm},
+        {6, HumanBodyBones.LeftLowerArm},
+        {7, HumanBodyBones.LeftHand},
+        {8, HumanBodyBones.LeftIndexProximal},
+
+        {9, HumanBodyBones.RightShoulder},
+        {10, HumanBodyBones.RightUpperArm},
+        {11, HumanBodyBones.RightLowerArm},
+        {12, HumanBodyBones.RightHand},
+        {13, HumanBodyBones.RightIndexProximal},
+
+        {14, HumanBodyBones.LeftUpperLeg},
+        {15, HumanBodyBones.LeftLowerLeg},
+        {16, HumanBodyBones.LeftFoot},
+        {17, HumanBodyBones.LeftToes},
+
+        {18, HumanBodyBones.RightUpperLeg},
+        {19, HumanBodyBones.RightLowerLeg},
+        {20, HumanBodyBones.RightFoot},
+        {21, HumanBodyBones.RightToes},
+    };
+
+    protected readonly Dictionary<int, KinectWrapper.NuiSkeletonPositionIndex> boneIndex2JointMap = new Dictionary<int, KinectWrapper.NuiSkeletonPositionIndex>
+    {
+        {0, KinectWrapper.NuiSkeletonPositionIndex.HipCenter},
+        {1, KinectWrapper.NuiSkeletonPositionIndex.Spine},
+        {2, KinectWrapper.NuiSkeletonPositionIndex.ShoulderCenter},
+        {3, KinectWrapper.NuiSkeletonPositionIndex.Head},
+
+        {5, KinectWrapper.NuiSkeletonPositionIndex.ShoulderLeft},
+        {6, KinectWrapper.NuiSkeletonPositionIndex.ElbowLeft},
+        {7, KinectWrapper.NuiSkeletonPositionIndex.WristLeft},
+        {8, KinectWrapper.NuiSkeletonPositionIndex.HandLeft},
+
+        {10, KinectWrapper.NuiSkeletonPositionIndex.ShoulderRight},
+        {11, KinectWrapper.NuiSkeletonPositionIndex.ElbowRight},
+        {12, KinectWrapper.NuiSkeletonPositionIndex.WristRight},
+        {13, KinectWrapper.NuiSkeletonPositionIndex.HandRight},
+
+        {14, KinectWrapper.NuiSkeletonPositionIndex.HipLeft},
+        {15, KinectWrapper.NuiSkeletonPositionIndex.KneeLeft},
+        {16, KinectWrapper.NuiSkeletonPositionIndex.AnkleLeft},
+        {17, KinectWrapper.NuiSkeletonPositionIndex.FootLeft},
+
+        {18, KinectWrapper.NuiSkeletonPositionIndex.HipRight},
+        {19, KinectWrapper.NuiSkeletonPositionIndex.KneeRight},
+        {20, KinectWrapper.NuiSkeletonPositionIndex.AnkleRight},
+        {21, KinectWrapper.NuiSkeletonPositionIndex.FootRight},
+    };
+
+    protected readonly Dictionary<int, List<KinectWrapper.NuiSkeletonPositionIndex>> specIndex2JointMap = new Dictionary<int, List<KinectWrapper.NuiSkeletonPositionIndex>>
+    {
+        {4, new List<KinectWrapper.NuiSkeletonPositionIndex> {KinectWrapper.NuiSkeletonPositionIndex.ShoulderLeft, KinectWrapper.NuiSkeletonPositionIndex.ShoulderCenter} },
+        {9, new List<KinectWrapper.NuiSkeletonPositionIndex> {KinectWrapper.NuiSkeletonPositionIndex.ShoulderRight, KinectWrapper.NuiSkeletonPositionIndex.ShoulderCenter} },
+    };
+
+
+    public void Awake()
+    {
+        // check for double start
+        if (bones != null)
+            return;
+
+        // inits the bones array
+        bones = new Transform[22];
+
+        // Initial rotations and directions of the bones.
+        initialRotations = new Quaternion[bones.Length];
+        initialLocalRotations = new Quaternion[bones.Length];
+
+        // Map bones to the points the Kinect tracks
+        MapBones();
+
+        // Get initial bone rotations
+        GetInitialRotations();
+    }
+
+
+    protected Quaternion Kinect2AvatarRot(Quaternion jointRotation, int boneIndex)
+    {
+        // Apply the new rotation.
+        return jointRotation * initialRotations[boneIndex];
+    }
+
+
+    // Converts Kinect position to avatar skeleton position, depending on initial position, mirroring and move rate
+    protected Vector3 Kinect2AvatarPos(Vector3 jointPosition)
+    {
+        float xPos = jointPosition.x * moveRate - xOffset;
+        float yPos = jointPosition.y * moveRate - yOffset;
+        float zPos = -jointPosition.z * moveRate - zOffset;
+
+        // If we are tracking vertical movement, update the y. Otherwise leave it alone.
+        return new Vector3(xPos, yPos, zPos);
+    }
+
+
+    protected void MoveAvatar(uint UserID)
+    {
+        if (bodyRoot == null || kinectManager == null)
+            return;
+        if (!kinectManager.IsJointTracked(UserID, (int)KinectWrapper.NuiSkeletonPositionIndex.HipCenter))
+            return;
+
+        // Get the position of the body and store it.
+        Vector3 trans = kinectManager.GetUserPosition(UserID);
+
+        // If this is the first time we're moving the avatar, set the offset. Otherwise ignore it.
+        if (!offsetCalibrated)
+        {
+            offsetCalibrated = true;
+
+            xOffset = trans.x * moveRate;
+            yOffset = trans.y * moveRate;
+            zOffset = -trans.z * moveRate;
+
+            if (offsetRelativeToSensor)
+            {
+                Vector3 cameraPos = Camera.main.transform.position;
+
+                float yRelToAvatar = (offsetNode != null ? offsetNode.transform.position.y : transform.position.y) - cameraPos.y;
+                Vector3 relativePos = new Vector3(trans.x * moveRate, yRelToAvatar, trans.z * moveRate);
+                Vector3 offsetPos = cameraPos + relativePos;
+
+                if (offsetNode != null)
+                {
+                    offsetNode.transform.position = offsetPos;
+                }
+                else
+                {
+                    transform.position = offsetPos;
+                }
+            }
+        }
+
+        // Smoothly transition to the new position
+        Vector3 targetPos = Kinect2AvatarPos(trans);
+        bodyRoot.localPosition = targetPos;
+    }
+
+
+    // Apply the rotations tracked by kinect to the joints.
+    protected void TransformBone(uint userId, KinectWrapper.NuiSkeletonPositionIndex joint, int boneIndex)
+    {
+        Transform boneTransform = bones[boneIndex];
+        if (boneTransform == null || kinectManager == null)
+            return;
+
+        int iJoint = (int)joint;
+        if (iJoint < 0)
+            return;
+
+        // Get Kinect joint orientation
+        Quaternion jointRotation = kinectManager.GetJointOrientation(userId, iJoint, false);
+        if (jointRotation == Quaternion.identity)
+            return;
+
+        boneTransform.rotation = Kinect2AvatarRot(jointRotation, boneIndex);
+    }
+
+
+    // Update the avatar each frame.
+    public void UpdateAvatar(uint UserID)
+    {
+        if (!transform.gameObject.activeInHierarchy)
+            return;
+
+        // Get the KinectManager instance
+        if (kinectManager == null)
+        {
+            kinectManager = KinectManager.Instance;
+        }
+
+        // move the avatar to its Kinect position
+        MoveAvatar(UserID);
+
+        for (var boneIndex = 0; boneIndex < bones.Length; boneIndex++)
+        {
+            if (!bones[boneIndex])
+                continue;
+
+            if (boneIndex2JointMap.ContainsKey(boneIndex))
+            {
+                KinectWrapper.NuiSkeletonPositionIndex joint = boneIndex2JointMap[boneIndex];
+                TransformBone(UserID, joint, boneIndex);
+            }
+            else if (specIndex2JointMap.ContainsKey(boneIndex))
+            {
+                // special bones (clavicles)
+                List<KinectWrapper.NuiSkeletonPositionIndex> alJoints = specIndex2JointMap[boneIndex];
+
+                if (alJoints.Count >= 2)
+                {
+                    //Vector3 baseDir = alJoints[0].ToString().EndsWith("Left") ? Vector3.left : Vector3.right;
+                    //TransformSpecialBone(UserID, alJoints[0], alJoints[1], boneIndex, baseDir, !mirroredMovement);
+                }
+            }
+        }
+    }
+
+    // If the bones to be mapped have been declared, map that bone to the model.
+    protected virtual void MapBones()
+    {
+        // make OffsetNode as a parent of model transform.
+        offsetNode = new GameObject(name + "Ctrl") { layer = transform.gameObject.layer, tag = transform.gameObject.tag };
+        offsetNode.transform.position = transform.position;
+        offsetNode.transform.rotation = transform.rotation;
+        offsetNode.transform.parent = transform.parent;
+
+        transform.parent = offsetNode.transform;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+
+        // take model transform as body root
+        bodyRoot = transform;
+
+        // get bone transforms from the animator component
+        var animatorComponent = GetComponent<Animator>();
+
+        for (int boneIndex = 0; boneIndex < bones.Length; boneIndex++)
+        {
+            if (!boneIndex2MecanimMap.ContainsKey(boneIndex))
+                continue;
+
+            bones[boneIndex] = animatorComponent.GetBoneTransform(boneIndex2MecanimMap[boneIndex]);
+        }
+    }
+
+    protected void GetInitialRotations()
+    {
+        // save the initial rotation
+        if (offsetNode != null)
+        {
+            initialPosition = offsetNode.transform.position;
+            initialRotation = offsetNode.transform.rotation;
+
+            offsetNode.transform.rotation = Quaternion.identity;
+        }
+        else
+        {
+            initialPosition = transform.position;
+            initialRotation = transform.rotation;
+
+            transform.rotation = Quaternion.identity;
+        }
+
+        for (int i = 0; i < bones.Length; i++)
+        {
+            if (bones[i] != null)
+            {
+                initialRotations[i] = bones[i].rotation; // * Quaternion.Inverse(initialRotation);
+                initialLocalRotations[i] = bones[i].localRotation;
+            }
+        }
+
+        // Restore the initial rotation
+        if (offsetNode != null)
+        {
+            offsetNode.transform.rotation = initialRotation;
+        }
+        else
+        {
+            transform.rotation = initialRotation;
+        }
+    }
+}
